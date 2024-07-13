55e34f1851322bf27a4dc5b8906620c24cbea737
akshitmittal20 <akshitmittal20@gmail.com>
2024-07-13 17:34:07 +0530

    cors

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const app = require('./app');
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
