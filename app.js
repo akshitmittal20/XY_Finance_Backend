f354bdcd67531a478875d42bf86da6bca30d34dd
akshitmittal20 <akshitmittal20@gmail.com>
2024-07-13 17:54:45 +0530

    cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const quoteRoutes = require('./routes/quoteRoutes');
 const tokenRoutes = require('./routes/tokenRoutes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', quoteRoutes);
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const app = require('./app');
-const cors = require('cors');
-
 const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
-
-app.use(cors());
 app.use(bodyParser.json());
 
 app.listen(port, () => {
